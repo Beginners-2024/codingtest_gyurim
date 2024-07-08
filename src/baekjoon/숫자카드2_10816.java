e83de411cc4e41787ed4204e353c9985c2d302e3
gyurim <gyurim1250@gmail.com>
2024-07-08 20:58:21 +0900

    10816 풀이 수정

--- src/baekjoon/숫자카드2_10816.java
+++ src/baekjoon/숫자카드2_10816.java
@@ -8,8 +8,8 @@ import java.util.*;
  *-10,000,000 <= 숫자 카드에 적힌 수 <= 10,000,000
  * 1 ≤ M ≤ 500,000
  *------------------------------------------------------------------------------------
- * 시간복잡도: O(숫자카드 개수 세기: NlogN + 쿼리 처리: MlogN) = O((N+M)logN)
- * 메모리: 198688 KB, 시간: 1816 ms
+ * 시간복잡도: O(n + m)
+ * 메모리: 199440 KB, 시간: 1316 ms
  **************************************************************************************/
 
 public class 숫자카드2_10816 {
@@ -18,7 +18,7 @@ public class 숫자카드2_10816 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
@@ -35,9 +35,10 @@ public class 숫자카드2_10816 {
 
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
+
+        for (int i = 0; i < m; i++) { // O(m)
             int num = Integer.parseInt(st.nextToken());
-            if (map.containsKey(num)) sb.append(map.get(num) + " ");
+            if (map.containsKey(num)) sb.append(map.get(num) + " "); // O(1)
             else sb.append("0 ");
         }
         System.out.println(sb);
