7d9a41c2907862229c9a181dceb5b03adea7741f
gyurim <gyurim1250@gmail.com>
2024-08-03 11:27:09 +0900

    2457 풀이 완료

--- src/baekjoon/공주님의_정원_2457.java
+++ src/baekjoon/공주님의_정원_2457.java
@@ -4,8 +4,14 @@ import java.io.*;
 import java.util.*;
 
 /**************************************************************************************
- * 2시간 이상 걸리고 있음 ㅠㅠ .. 토요일에 이어서 풀어야지
+ * 1 ≤ N ≤ 100,000
+ *
+ * [조건]
+ * 1. 공주가 가장 좋아하는 계절인 3/1 ~ 11/30 까지 매일 꽃이 1가지 이상 피어있어야함
+ * 2. 정원이 넓지 않으므로 정원에 심는 꽃들의 수를 가능한 적게 함
  *------------------------------------------------------------------------------------
+ * 시간복잡도: O(N)
+ * 메모리: 52544 KB, 시간: 516 ms
  **************************************************************************************/
 
 public class 공주님의_정원_2457 {
@@ -14,35 +20,21 @@ public class 공주님의_정원_2457 {
     private static int n;
 
     private static class Date implements Comparable<Date> {
-        int sMonth;
-        int sDay;
-        int eMonth;
-        int eDay;
+        int start;
+        int end;
 
-        Date(int startMonth, int startDay, int endMonth, int endDay) {
-            this.sMonth = startMonth;
-            this.sDay = startDay;
-            this.eMonth = endMonth;
-            this.eDay = endDay;
+        Date(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
 
-        // 1순위: startMonth가 작은
-        // 2순위: startDay가 작은
-        // 3순위: endMonth가 작은
-        // 4순위: endDay가 작은
+        // 1순위: start 날짜가 작은
+        // 2순위: end 날짜가 큰
         @Override
         public int compareTo(Date o) {
-            if (this.sMonth != o.sMonth)
-                return Integer.compare(this.sMonth, o.sMonth);
-            else {
-                if (this.sDay != o.sDay)
-                    return Integer.compare(this.sDay, o.sDay);
-                else {
-                    if (this.eMonth != o.eMonth)
-                        return Integer.compare(this.eMonth, o.eMonth);
-                    else return Integer.compare(this.eDay, o.eDay);
-                }
-            }
+            if (this.start != o.start)
+                return Integer.compare(this.start, o.start);
+            else return Integer.compare(o.end, this.end);
         }
     }
 
@@ -55,7 +47,7 @@ public class 공주님의_정원_2457 {
         flowers = new Date[n];
         visit = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // O(N)
             st = new StringTokenizer(br.readLine());
 
             int sMonth = Integer.parseInt(st.nextToken());
@@ -63,7 +55,7 @@ public class 공주님의_정원_2457 {
             int eMonth = Integer.parseInt(st.nextToken());
             int eDay = Integer.parseInt(st.nextToken());
 
-            flowers[i] = new Date(sMonth, sDay, eMonth, eDay);
+            flowers[i] = new Date(sMonth * 100 + sDay, eMonth * 100 + eDay);
         }
 
         Arrays.sort(flowers);
@@ -71,73 +63,40 @@ public class 공주님의_정원_2457 {
         System.out.println(selectFlower());
     }
 
-    private static Date princess = new Date(3, 1, 11, 30); // 공주 선호 날짜
-
     private static int selectFlower() {
-        int left = 0;
+        int count = 0;
 
-        // 1. 최적의 시작 left 설정 (공주가 선호하는 날짜 기간 안에 있어야 함)
-        while (left < n) {
-            Date start = flowers[left];
+        int start = 301;    // 다음 꽃이 시작되어야하는 최소 시작일을 담고 있음 (즉, 이전 꽃의 종료일)
+        int endDay = 1201;
+        int index = 0;
+        int max = 0;        // 최적의 다음 꽃을 찾기 위해, 현 시점의 가장 늦은 종료일을 담고 있음 (즉, 더 최적의 꽃이 나온다면, 해당 꽃의 종료일로 갱신됨)
 
-            if (princess.sMonth > start.sMonth) left++;
-            else if (princess.sMonth == start.sMonth) {
-                if (princess.sDay > start.sDay) left++;
-                else {
-                    left -= 1;
+        while (start < endDay) { // O(N)
+            boolean isFound = false;
+
+            for (int i = index; i < n; i++) {
+                if (flowers[i].start > start) {
+                    // 1. 3/1에 꽃이 필 수 없다면, break
+                    // 2. 이전 꽃의 종료일(start)보다 현재 i번째 꽃의 시작일이 늦다면, 간격이 생겼으므로, break
                     break;
                 }
-            } else {
-                left -= 1;
-                break;
-            }
-        }
-        System.out.println("left: " + left);
 
-        if (left == n) return 0;
-
-        int count = 1;
-        int right = left + 1;
-
-        while (right < n) {
-            Date prev = flowers[left];
-            Date next = flowers[right];
-
-            printDate(prev);
-            printDate(next);
-            System.out.println();
-
-            if (!checkDate(prev, next) && checkDate(next, princess)) {
-                left = right - 1;
-                System.out.println(left + " " + right);
-                count++;
-            } else {
-                // 현재 right 인덱스부터, right 고려 대상임 -> 따라서, 최적의 right를 찾아야 함
-                right++;
-
-                if (right == n) {
-                    count++;
+                if (max < flowers[i].end) {
+                    // 현재 i번째 꽃의 종료일이 이전 꽃 종료일(max)보다 늦다면, 다음 꽃을 찾았다는 뜻! => max 갱신
+                    isFound = true;
+                    max = flowers[i].end;
+                    index = i + 1;
                 }
             }
+
+            if (isFound) {
+                // 다음 꽃을 찾았다면, start에 직전 꽃의 종료일을 넣어줌
+                start = max;
+                count++;
+            } else break;
         }
+
+        if (max < endDay) return 0; // 11/30까지 꽃이 필 수 없다면, 조건 만족 X
         return count;
-    }
-
-    private static void printDate(Date date) {
-        System.out.println(date.sMonth + "월 " + date.sDay + "일 ~ " + date.eMonth + "월 " + date.eDay + "일");
-    }
-
-    private static boolean checkDate(Date prev, Date next) { // 두 날짜 사이에 빈 간격이 있으면 안되기에 이를 체크
-        if (prev.eMonth < next.sMonth) return false;
-        else if (prev.eMonth == next.sMonth) {
-            if (prev.eDay < next.sDay) return false;
-        }
-
-        if (prev.sMonth > next.eMonth) return false;
-        else if (prev.sMonth == next.eMonth) {
-            if (prev.sDay > next.eDay) return false;
-        }
-
-        return true;
     }
 }
